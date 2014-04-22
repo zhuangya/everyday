01bb3e3f656f72010661f73e2276e26b9d2c30d2
Zhuang Ya <zhuangya@gmail.com>
2014-04-23 01:41:11 +0800

    chore: add rendening page

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 'use strict';
 
+/**
+ * all kinds of middlewares
+ */
+
 var logger = require('morgan');
 var express = require('express');
-var app = express();
+var jade = require('jade');
+var serveStatic = require('serve-static');
+
+/**
+ * config
+ */
 
 var webConfig = require('config').express;
 
+var app = express();
+
+/**
+ * app settings
+ */
 app.set('title', 'everyday');
 app.set('view engine', 'jade');
 
-app.use(logger());
+//app.use(logger());
 app.use(require('response-time')());
 app.use(require('errorhandler')());
+app.use(serveStatic(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('hey');
+  res.render('index', {
+    title: 'Everyday'
+  });
 });
 
-app.listen(webConfig.port || 3000, function () {
-  console.log(arguments);
-});
+app.listen(webConfig.port || 3000);
